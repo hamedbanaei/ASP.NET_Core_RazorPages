cb788dce914aceef27f3efd6549dd761988a2d9c
HamedBanaei <hamedbanaei1982@gmail.com>
2022-12-14 10:01:11 +0330

    ExLesson01_AlgorithmsBenchmark Added

---
+++ ExLesson01_AlgorithmsBenchmark/CryptographyAlgorithmsBenchmark/HashAlgorithms.cs
@@ -0,0 +1,44 @@
+﻿namespace CryptographyAlgorithmsBenchmark
+{
+
+    [BenchmarkDotNet.Attributes.MemoryDiagnoser]
+    public class HashAlgorithms : System.Object
+    {
+        public HashAlgorithms()
+        {
+            System.Security.Cryptography.RandomNumberGenerator.Create().GetNonZeroBytes(InputValue);
+        }
+
+        public byte[] InputValue = new byte[5000];
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public void MD5HashAlgorithm()
+        {
+            System.Security.Cryptography.MD5.HashData(InputValue);
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public void Sha1HashAlgorithm()
+        {
+            System.Security.Cryptography.SHA1.HashData(InputValue);
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public void Sha256HashAlgorithm()
+        {
+            System.Security.Cryptography.SHA256.HashData(InputValue);
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public void Sha384HashAlgorithm()
+        {
+            System.Security.Cryptography.SHA384.HashData(InputValue);
+        }
+
+        [BenchmarkDotNet.Attributes.Benchmark]
+        public void Sha512HashAlgorithm()
+        {
+            System.Security.Cryptography.SHA512.HashData(InputValue);
+        }
+    }
+}
